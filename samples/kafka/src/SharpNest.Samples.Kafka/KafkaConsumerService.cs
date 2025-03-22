1a52d26d80969e22141cfe6b4cffc3f35d770b29
Anastas Kostov <Anastas.Kostov@tbs.tech>
2025-03-22 22:46:28 +0200

    kafka sample

---
+++ samples/kafka/src/SharpNest.Samples.Kafka/KafkaConsumerService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SharpNest.Kafka.Core.Abstractions;
+
+namespace KafkaWebApiDemo.Services;
+
+public class KafkaConsumerService(ISubscriber subscriber, ILogger<KafkaConsumerService> logger) : BackgroundService
+{
+    private readonly ISubscriber _subscriber = subscriber;
+    private readonly ILogger<KafkaConsumerService> _logger = logger;
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        try
+        {
+            await _subscriber.SubscribeAsync(
+                "messages-topic",
+                async message =>
+                {
+                    _logger.LogInformation(
+                        "Message received: Topic={Topic}, Key={Key}, Value={Value}",
+                        message.Topic,
+                        message.Key,
+                        message.Value);
+
+                    await Task.CompletedTask;
+                },
+                "consumer-group-1",
+                stoppingToken);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error in Kafka consumer service");
+        }
+    }
+}
